e19c65854fab2aff0e55fd5eda4b969a8517f62c
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-03-29 19:18:30 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  const handleDoubleClick = (event) => {
+    event.preventDefault();
+    console.log("I was double clicked");
+  };
+  const handleSingleClick = (event) => {
+    event.preventDefault();
+    console.log("I was not double clicked");
+  };
 
-  const handleDoubleClick = (event) =>{
-  
-  }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        onDoubleClick={handleDoubleClick}
+        onClick={handleSingleClick}
+        id="dblclick-btn"
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
